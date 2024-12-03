2a763ff02a98e2120ddd6e18f2b77faf6ae8f6c1
Jordanmaarten <124160@student.drenthecollege.nl>
2024-12-03 10:27:51 +0100

    added bands, band members and epk tables to migrations

---
+++ database/migrations/2024_12_02_121154_create_band_members_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('band_members', function (Blueprint $table) {
+            $table->bigInteger('band_id');
+            $table->string('band_name');
+            $table->bigInteger('member_id');
+            $table->string('member_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('band_members');
+    }
+};
---
+++ database/migrations/2024_12_02_121203_create_epk_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('epks', function (Blueprint $table) {
+            $table->id('epk_id');
+            $table->string('epk_name');
+            $table->bigInteger('band_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('epk');
+    }
+};
