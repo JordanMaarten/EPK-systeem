a96b7fd859a0831f140fc9723af81c5f70412a58
Jordan <124160@student.dcterra.nl>
2024-12-11 13:57:37 +0100

    added Table, TableHeader, TableData components

---
+++ resources/js/Components/Table.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <table class="table-auto border-collapse w-full text-sm">
+        <thead>
+            <slot name="thead"/>
+        </thead>
+        <tbody class="bg-white dark:bg-slate-800">
+            <slot name="tbody"/>
+        </tbody>
+    </table>
+</template>
---
+++ resources/js/Components/TableData.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+        <slot/>
+    </td>
+</template>
