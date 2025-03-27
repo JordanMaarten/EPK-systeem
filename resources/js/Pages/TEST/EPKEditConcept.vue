ea0091423f0377844bf35cfffe8cf35519d2f6dc
Jordan <124160@student.dcterra.nl>
2025-03-27 11:48:32 +0100

    transfer to pc for loa

--- resources/js/Pages/TEST/EPKEditConcept.vue
+++ resources/js/Pages/TEST/EPKEditConcept.vue
@@ -60,9 +60,9 @@ onMounted(() => {
         saveHTML();
     });
 
-    document.getElementById("addRow").addEventListener("click", (event) => {
-        CMS.appendRow(content.value.getElementsByTagName("body")[0]);
-    });
+    // document.getElementById("addRow").addEventListener("click", (event) => {
+    //     CMS.addRow(content.value.getElementsByTagName("body")[0]);
+    // });
 
     // use later for automatic saving
     setInterval(function () {
@@ -90,14 +90,26 @@ onMounted(() => {
             </div>
         </template>
         <div id="main" class="px-[8em]">
-            <div id="content" ref="content">
-                <body>
-                    <testComponent></testComponent>
-                </body>
-            </div>
+            <div id="content" ref="content"></div>
             <Divider/>
             <div class="flex justify-center">
-                <SecondaryButton id="addRow" class="w-[36em] justify-center text-white !bg-emerald-500 !hover:bg-emerald-600">Add new row +</SecondaryButton>
+                <SecondaryButton @click="CMS.showElement('rowSelect')" class="w-[36em] justify-center text-white !bg-emerald-500 !hover:bg-emerald-600">Add new row +</SecondaryButton>
+            </div>
+        </div>
+        <div id="overlay" class="hidden absolute top-0 left-0 w-full h-full bg-slate-200/75 px-[8em]" aria-hide>
+            <div @click="CMS.hideElementsInId('overlay')" class="z-10 absolute top-0 left-0 w-full h-full"></div>
+            <div class="relative flex w-full h-full">
+                <div class="hidden z-50 flex relative justify-center gap-5 p-5 m-auto border-2 border-black border-dashed rounded" aria-hide aria-trigger="rowSelect">
+                    <div @click="CMS.addRow('content')" class="bg-white w-[7em] h-[7em]"></div>
+                    <div class="bg-white w-[7em] h-[7em]"></div>
+                    <div class="bg-white w-[7em] h-[7em]"></div>
+                    <div class="bg-white w-[7em] h-[7em]"></div>
+                    <SecondaryButton @click="CMS.hideElementsInId('overlay')" class="absolute right-[-10px] top-[-10px] !p-1 text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="-1 0 25 25" fill="none">
+                            <path d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z" fill="#0F0F0F"/>
+                        </svg>
+                    </SecondaryButton>
+                </div>
             </div>
         </div>
     </AuthenticatedLayout>
--- resources/js/Scripts/CMS.js
+++ resources/js/Scripts/CMS.js
@@ -2,12 +2,86 @@
 
 export const hooktest = "successfully imported CMS.js";
 
-export function appendRow(target) {
-    console.log(target);
+// Element references
+window.onload = (event) => {
+    window.addEventListener("click", (event) => {
+        // console.log(event.target);
+    })
+    console.log(document.readyState);
+};
 
-    let row = document.createElement("div");
+// Elements
 
-    row.classList.add("row");
+const CMS_elements = {
+    element_selector : () => {
+        let el = document.createElement("div"); 
+        //el.classList.add("relative", "flex", "justify-between");
 
-    target.appendChild(row);
+        let elements_keys = Object.keys(elements);
+        return el;
+    },
+}
+
+const elements = {
+    row : () => {let el = document.createElement("div"); el.classList.add("relative", "flex", "justify-between"); return el;},
+}
+
+const global_elements = {
+    group : {
+        text_block : () => {
+            let el = document.createElement("span"); return el;
+        }
+    }
+}
+
+// Methods
+
+export function showElement(target_name) {
+    let targets = document.querySelectorAll("[aria-hide][aria-trigger='" + target_name + "']");
+
+    targets.forEach(t => {
+        while (t.closest(".hidden[aria-hide]")) {
+            t.closest(".hidden[aria-hide]").classList.remove("hidden");
+        }
+        t.classList.remove("hidden");
+    });
+}
+
+export function hideElement(target_name) {
+    let targets = document.querySelectorAll("[aria-hide][aria-trigger=" + target_name + "]");
+
+    targets.forEach(t => {
+        t.classList.add("hidden");
+    });
+}
+
+export function hideElementsInId(scope_id) {
+    let scope = document.getElementById(scope_id);
+    let targets = scope.querySelectorAll("[aria-hide]");
+    targets.forEach(t => {
+        if (scope.hasAttribute("aria-hide")) {
+            scope.classList.add("hidden");
+        }
+        t.classList.add("hidden");
+    });
+}
+
+export function getCMSElement(key) { 
+    return elements[key]();
+}
+
+export function addRow(target_id) {
+    let target = document.getElementById(target_id);
+    let row = getCMSElement("row");
+    let elements_keys = Object.keys(global_elements);
+
+    console.log(elements_keys);
+    
+}
+
+export function appendElement(target_id, key) {
+    let target = document.getElementById(target_id);
+    let element = getCMSElement(key);
+    
+    target.insertAdjacentElement("afterend", element);
 }
